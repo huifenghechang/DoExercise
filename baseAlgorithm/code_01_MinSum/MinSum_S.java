a98ab7d3545fbaef381ce5381102dffd136bd79c
苏小乐 <1160443855@qq.com>
2019-01-14 14:44:56 +0800

    Initial commit 算法刷题笔记

---
+++ baseAlgorithm/code_01_MinSum/MinSum_S.java
@@ -0,0 +1,41 @@
+package code_01_MinSum;
+
+public class MinSum_S {
+    public static void main(String[] args){
+        int[] nums = {1,3,4,2,5};
+        if (!validity(nums)){
+            System.out.println("The array is inValidity!");
+        }else {
+            System.out.println("The minMax is"+minMax(nums,nums.length));
+        }
+    }
+
+    public static boolean validity(int[] args){
+        if (args.length<1 || args==null){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public static int minMax(int[] args,int length){
+        int sum = 0;
+        if (length == 1){
+            return 0;
+        }
+        int[] count = new int[length-1];
+        for(int i=0;i<length;i++){
+            int num = args[i];
+            for(int j=i+1;j<length;j++){
+                if (args[j]>num){
+                    count[i]++;
+                }
+            }
+        }
+
+        for (int i=0;i<length-1;i++){
+            sum += args[i]*count[i];
+        }
+        return sum;
+    }
+}
---
+++ baseAlgorithm/code_02_BubbleSort/BubbleSort.java
@@ -0,0 +1,39 @@
+package code_02_BubbleSort;
+
+public class BubbleSort {
+
+    public static void bubble(int[] arr){
+        if(arr==null || arr.length<2){
+            return;
+        }
+        for(int end=arr.length-1;end>0;end--){
+            for(int i=0;i<end;i++){
+                if (arr[i]>arr[i+1]){
+                    swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[]arr,int i,int j){
+        int temp =arr[i];
+        arr[i]= arr[j];
+        arr[j]=temp;
+    }
+
+    public static void PrintfArray(int[] array){
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]+"、");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int[] numbers = {1,3,2,5,7};
+        System.out.print("The origin is:");
+        PrintfArray(numbers);
+        bubble(numbers);
+        System.out.print("The sorted is:");
+        PrintfArray(numbers);
+
+    }
+}
