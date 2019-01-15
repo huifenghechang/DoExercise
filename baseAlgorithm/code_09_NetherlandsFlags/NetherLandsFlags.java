cf307f68bbd242a4a12d54d836fcc2d35813cb5c
苏小乐 <1160443855@qq.com>
2019-01-15 15:47:54 +0800

    荷兰国旗问题

---
+++ baseAlgorithm/code_09_NetherlandsFlags/NetherLandsFlags.java
@@ -0,0 +1,28 @@
+package code_09_NetherlandsFlags;
+
+import static code_02_bubbleSort.BubbleSort.swap;
+import static code_02_bubbleSort.BubbleSort_Test.printArray;
+
+public class NetherLandsFlags {
+
+    public static void partition(int[] arr, int l, int r,int p){
+        int less = l-1;
+        int more = r+1;
+        int cur = l;
+        while (cur < more){
+            if (arr[cur] < p){
+                swap(arr,cur++,++less);
+            }else if (arr[cur] > p){
+                swap(arr,cur,--more);
+            }else {
+                cur++;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {4,5,3,1,9,25,7,8,6};
+        partition(arr,0,arr.length-1,6);
+        printArray(arr);
+    }
+}
