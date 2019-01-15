6ac3275bb148896bde5a306e35711114725fb7d8
苏小乐 <1160443855@qq.com>
2019-01-15 17:11:33 +0800

    快速排序，包含递归和划分两种思想在里面。

---
+++ baseAlgorithm/code_10_quickSort/QuickSort_S01.java
@@ -0,0 +1,50 @@
+package code_10_quickSort;
+
+import static utils.utils.PrintfArray;
+import static utils.utils.swap;
+
+public class QuickSort_S01 {
+    public static void quickSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+        quickSort(arr,0,arr.length-1);
+    }
+
+    public static void quickSort(int[] arr,int l ,int r){
+        if( l < r){
+            int[] p = partition(arr,l,r);
+            quickSort(arr,l,p[0]-1);
+            quickSort(arr,p[1]+1,r);
+        }
+    }
+
+    public static int[] partition(int[] arr,int l ,int r){
+        int less = l-1;
+        int more = r;
+        int cur = l;
+
+        while (cur < more){
+            if(arr[cur] < arr[r]){
+                swap(arr,cur++,++less);
+            }else if(arr[cur] > arr[r]){
+                swap(arr,cur,--more);
+            }else {
+                cur++;
+            }
+        }
+        swap(arr,more,r); // 这一行原来没有，主要的作用，是将枢轴放置在数组的之中。必须要这么做！
+        //返回的是等于区间的下标值。
+        return new int[]{less+1,more};
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,3,4,2,9,5};
+        System.out.print("The origin is:");
+        PrintfArray(arr);
+        quickSort(arr);
+        System.out.print("The sorted is:");
+        PrintfArray(arr);
+
+    }
+}
