5677981e2290210bc1e57dfb4b404b4f6e9a9377
苏小乐 <1160443855@qq.com>
2019-01-18 12:26:45 +0800

    netherLandsFlags-Review

---
+++ baseAlgorithm/code_09_NetherlandsFlags/NetherLandsFlags_S01.java
@@ -0,0 +1,52 @@
+package code_09_NetherlandsFlags;
+
+import utils.NLFCompatator;
+
+import static utils.Comparator.*;
+import static utils.utils.*;
+
+public class NetherLandsFlags_S01 {
+
+    public static void netherlandsFlags(int[] arr,int left, int right,int num){
+        if (arr == null ){
+            return;
+        }
+        //采用分区的思想来处理荷兰国旗问题
+        int less = left -1;
+        int more = right + 1;
+        int cur = left;
+
+        while (cur < more){ // 写成了less < more
+            if (arr[cur] < num){
+                swap(arr,cur++,++less);
+            }else if (arr[cur] > num){
+                swap(arr,cur,--more);
+            }else {
+                cur++;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int testTime = 50000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = duplicateArray(arr1);
+            netherlandsFlags(arr1,0,arr1.length-1,50);
+            NLFCompatator.partition(arr2,0,arr2.length-1,50);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!you passed!" : "you passed!");
+
+        int[] arr = generateRandomArray(10,100);
+        printfArray(arr);
+        netherlandsFlags(arr,0,arr.length-1,50);
+        printfArray(arr);
+    }
+}
--- baseAlgorithm/code_10_quickSort/QuickSort_S02.java
+++ baseAlgorithm/code_10_quickSort/QuickSort_S02.java
@@ -3,7 +3,7 @@ package code_10_quickSort;
 import static utils.utils.printfArray;
 import static utils.utils.swap;
 
-public class QuickSort_S01 {
+public class QuickSort_S02 {
     public static void quickSort(int[] arr){
         if(arr == null || arr.length < 2){
             return;
@@ -13,7 +13,6 @@ public class QuickSort_S01 {
 
     public static void quickSort(int[] arr,int l ,int r){
         if( l < r){
-            swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
             int[] p = partition(arr,l,r);
             quickSort(arr,l,p[0]-1);
             quickSort(arr,p[1]+1,r);
@@ -22,25 +21,25 @@ public class QuickSort_S01 {
 
     public static int[] partition(int[] arr,int l ,int r){
         int less = l-1;
-        int more = r;
+        int more = r+1;
+        int num =arr[r];
         int cur = l;
 
         while (cur < more){
-            if(arr[cur] < arr[r]){
+            if(arr[cur] < num){
                 swap(arr,cur++,++less);
-            }else if(arr[cur] > arr[r]){
+            }else if(arr[cur] > num){
                 swap(arr,cur,--more);
             }else {
                 cur++;
             }
         }
-        swap(arr,more,r); // 这一行原来没有，主要的作用，是将枢轴放置在数组的之中。必须要这么做！
         //返回的是等于区间的下标值。
-        return new int[]{less+1,more};
+        return new int[]{less+1,more-1};
     }
 
     public static void main(String[] args){
-        int[] arr = {1,3,4,2,9,5,56};
+        int[] arr = {1,3,4,2,9,5};
         System.out.print("The origin is:");
         printfArray(arr);
         quickSort(arr);
