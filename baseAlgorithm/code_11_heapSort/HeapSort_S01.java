5a8a04771133fa3e373e1e46860d6f305b75a5cf
苏小乐 <1160443855@qq.com>
2019-01-19 10:49:52 +0800

    heapsort-review

--- baseAlgorithm/code_11_heapSort/HeapSort_S01.java
+++ baseAlgorithm/code_11_heapSort/HeapSort_S01.java
@@ -4,47 +4,43 @@ import static utils.Comparator.*;
 import static utils.utils.printfArray;
 import static utils.utils.swap;
 
-public class HeapSortComparator {
-
+public class HeapSort_S01 {
     public static void heapSort(int[] arr){
-        if (arr == null || arr.length < 2){
+        if ( arr == null || arr.length < 2){
             return;
         }
 
-        for (int i = 0 ;i < arr.length; i++){
+        for (int i=0; i < arr.length; i++){
             heapInsert(arr,i);
         }
-        int size = arr.length;
-        swap(arr,0,--size);
-        while (size > 0){
-            heapify(arr,0,size);
-            swap(arr,0,--size);
-        }
 
+        int heapSize = arr.length;
+        swap(arr,0,--heapSize);
+        while ( heapSize > 0){
+            heapify(arr,0,heapSize);
+            swap(arr,0,--heapSize);
+        }
     }
 
-    // 将数组调整为大根堆,将第一个数视为一个大根堆.
-    // 向上调整，形成大根堆
     public static void heapInsert(int[] arr,int index){
-        while (arr[index] > arr[(index - 1)/2]){
-            swap(arr,index,(index - 1)/2);
-            index = (index - 1)/2;
+        while (arr[(index-1)/2] < arr[index]){
+            swap(arr,(index-1)/2,index);
+            index = (index-1)/2;
         }
     }
 
-    // 将堆顶元素变换后，向下调整，成为大根堆
-    public static void heapify(int[] arr, int index, int heapSize){
-        int left = index * 2 + 1;
-        while (left < heapSize){ // 错误写法： index < heapSize
-            int largest = left + 1 < heapSize && arr[left + 1] > arr[left]? left + 1: left;
-            largest = arr[largest] > arr[index]? largest : index;
-
-            if (largest == index){
+    // 将根换为一个新的数，向下调整为一个大根堆
+    public static void heapify(int[] arr,int index,int heapSize){
+        int left = 2 * index + 1;
+        while (left < heapSize){
+            int largest = left + 1 < heapSize && arr[left+1] > arr[left] ? left + 1 : left;
+            largest =  arr[index] < arr[largest] ? largest : index;
+            swap(arr,index,largest);
+            if(largest == index){
                 break;
             }
-            swap(arr,index,largest);
             index = largest;
-            left = index * 2 + 1; //此行代码代表着继续向下调整。
+            left = 2 * index + 1;
         }
     }
 
