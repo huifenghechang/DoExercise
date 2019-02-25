3ce6867b00f0cf2f47f0d5ceca703be9c64068eb
苏小乐 <1160443855@qq.com>
2019-02-25 16:48:10 +0800

    搜索二叉树与完全二叉树的判断

---
+++ base_03_Tree/code_05_IsBSTAndCBT/IsBSTAndCBT.java
@@ -0,0 +1,113 @@
+package code_05_IsBSTAndCBT;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsBSTAndCBT {
+
+    public static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value = value;;
+        }
+    }
+
+    // 判断是否为“搜索二叉树”
+
+    public static boolean isSBT(Node head){
+        return  false;
+    }
+
+
+    /*宽度遍历二叉树：
+    * 采用队列的结构来实现：
+    * */
+    public static void breadthTraversal(Node head){
+        if (head != null){
+            Queue<Node> queue = new LinkedList<Node>();
+            queue.add(head);
+
+            while (!queue.isEmpty()){
+                head = ((LinkedList<Node>) queue).pop();
+                System.out.print(head.value + " , ");
+
+                if (head.left != null){
+                    queue.add(head.left);
+                }
+
+                if (head.right != null){
+                    queue.add(head.right);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // 宽度遍历，判断一棵树是否为完全二叉树
+    public static boolean isCBT(Node head){
+        if (head == null){
+            return true;
+        }
+
+        // 层次遍历二叉树
+        Queue<Node> queue = new LinkedList<Node>();
+        boolean leaf = false;
+        Node left = null;
+        Node right = null;
+        queue.offer(head);
+
+        while (!queue.isEmpty()){
+                Node node =  queue.poll();
+                left = node.left;
+                right = node.right;
+
+                // leaf && (left != null ||right != null 判断条件，对应第二种情况。
+                if ((leaf && (left != null ||right != null))
+                        ||
+                        (left== null && right != null)) {
+                    return false;
+                 }
+
+                // 宽度遍历代码，将其加入到队列中~
+                if (left != null){
+                    queue.offer(left);
+                }
+                if (right != null){
+                    queue.offer(right);
+                }
+
+                // 此条判断对应情况2，此时，每个后续每个节点必须为叶子节点
+                if (left == null || right ==null){
+                    leaf = true;
+                }
+            }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Node head1 = new Node(1);
+        head1.left = new Node(2);
+        head1.right = new Node(3);
+        head1.left.left = new Node(4);
+        head1.left.right = new Node(5);
+        head1.right.left = new Node(6);
+        head1.right.right = new Node(7);
+        System.out.println("============层次遍历=============");
+        breadthTraversal(head1);
+
+        Node head2 = new Node(4);
+        head2.left = new Node(2);
+        head2.right = new Node(6);
+        head2.left.left = new Node(1);
+        head2.left.right = new Node(3);
+        head2.right.left = new Node(5);
+        System.out.println(isCBT(head2));
+
+
+
+    }
+
+}
